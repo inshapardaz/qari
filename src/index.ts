50811c09874fe16760501a897f1de6ad2116a4fd
Omar Faruk <omar.faruk@outlook.com>
2026-08-09 17:29:01 +0100

    Added publishing

---
+++ src/index.ts
@@ -0,0 +1,8 @@
+/**
+ * Public entry point for the `qari` package.
+ */
+
+export * from './components/Reader';
+export * as models from './models';
+export * as interfaces from './interfaces';
+export * from './i18n';
